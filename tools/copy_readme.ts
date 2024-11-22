e5c9b23407280bf4b074f52afd316e086a88f9c3
Ivan Herman <ivan@ivan-herman.net>
2024-11-22 15:55:03 +0100

    New documentation

---
+++ tools/copy_readme.ts
@@ -0,0 +1,29 @@
+/* *************************************************************************
+Simple tools to put a copy of the README.md file into the module documentation
+part of index.ts. By running `deno doc` on the resulting file the generated
+documentation will also generate content based on the README.md file
+(which is, otherwise, ignored by `deno doc`).
+
+I hope that, at some point, this tools will become unnecessary, and `deno doc`
+will be comparable to, say, typedoc...
+**************************************************************************** */
+
+
+const readme: string[] = Deno.readTextFileSync('./README.md').split('\n');
+const index: string[] = Deno.readTextFileSync('./index.ts').split('\n');
+
+const result: string[] = [];
+
+for (const indexLine of index) {
+    // Locate the anchor for the readme file:
+    if (indexLine.startsWith(" * @module")) {
+        // copy the content of the readme file, preceded with the documentation mark
+        for (const readmeLine of readme) {
+            result.push(` * ${readmeLine}`);
+        }
+    }
+    result.push(indexLine);
+}
+
+console.log(result.join('\n'));
+
