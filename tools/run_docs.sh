22bf02aaec1f9741349259d64fab4030c84e47ca
Ivan Herman <ivan@ivan-herman.net>
2024-11-29 16:49:55 +0100

    Maybe it works now for the documentation...

--- tools/run_docs.sh
+++ tools/run_docs.sh
@@ -5,4 +5,5 @@ cp index.ts multikey_webcrypto.ts
 deno run -A tools/copy_readme.ts
 deno doc --html --name="Multikey/WebCrypto API" index.ts
 mv multikey_webcrypto.ts index.ts
+(cd docs; mv \~ tilde)
 deno run -A tools/remove_tilde.ts
