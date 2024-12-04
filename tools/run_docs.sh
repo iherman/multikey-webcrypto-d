44e0a94fedb1505e5d6c5c20cd97ce0d832f292a
Ivan Herman <ivan@ivan-herman.net>
2024-12-04 11:50:31 +0100

    Add missing doc files

--- tools/run_docs.sh
+++ tools/run_docs.sh
@@ -3,6 +3,6 @@
 (cd docs; /bin/rm -rf *)
 cp index.ts multikey_webcrypto.ts
 deno run -A tools/copy_readme.ts
-deno doc --html --name="Multikey/WebCrypto API" index.ts
+deno doc --html --name="Multikey/WebCrypto API" index.ts lib/*
 mv multikey_webcrypto.ts index.ts
 (cd docs; touch .nojekyll)
